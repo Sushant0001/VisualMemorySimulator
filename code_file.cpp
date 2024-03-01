b57c9ff5e5271c854f3df9e9e026c252c7f2d636
Sushant Bhalerao <sushantb496@gmail.com>
2024-03-01 22:37:54 +0530

    Add files via upload

---
+++ code_file.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a[100][100];
+    for(int i=0;i<100;i++)
+    {
+        for(int j=0;j<100;j++)
+        {
+        printf("%d",a[i][j]);
+        }
+    }
+}
---
+++ cpp_file_creator.py
@@ -0,0 +1,6 @@
+
+def create_code_file(text):
+	file = open("code_file.cpp",'w')
+	file.write(str(text))
+	file.close()
+
